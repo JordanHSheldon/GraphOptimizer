baf21b55e350350f2fde6f1181867ad25ca45f9a
jordansheldon <Jordanhsheldon@gmail.com>
2024-06-19 21:52:38 -0400

    added file to generate points

--- Main/src/main/java/GraphOptimizer/Main/MainApplication.java
+++ Main/src/main/java/GraphOptimizer/Main/MainApplication.java
@@ -2,7 +2,13 @@ package GraphOptimizer.Main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 @SpringBootApplication
 public class MainApplication {
 
@@ -10,4 +16,9 @@ public class MainApplication {
 		SpringApplication.run(MainApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String GraphPoints(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		PointsService points = new PointsService(10);
+		return String.format("<html><h1>Html</h1></html>");
+	}
 }
